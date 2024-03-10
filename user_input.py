01515272da9c8db99aadc58b075653038b4a4bd8
McLito <litorohcalebwanjala@gmail.com>
2024-03-11 02:00:42 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name =input( 'Hey there, what is your name?')
+age = input( 'how old are you?')
+location = input( 'where do you come from?')
+print('Hello {}, please confirm by smilling that you are {}years old and that you come from {}, thank you!'.format(name,age,location))
